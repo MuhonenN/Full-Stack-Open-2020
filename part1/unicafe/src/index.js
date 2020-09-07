3ec8d11de0d7d2ac2c7efb78ebf13136a2732cb8
MuhonenN <miuhonen@gmail.com>
2020-09-07 13:28:34 +0300

    neljäs

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -15,17 +15,34 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [all, setAll] = useState(0)
+
+  const handleGoodClick = () => {
+    setAll(all + 1)
+    setGood(good + 1)
+  }
+  const handleNeutralClick = () => {
+    setAll(all + 1)
+    setNeutral(neutral + 1)
+  }
+  const handleBadClick = () => {
+    setAll(all + 1)
+    setBad(bad + 1)
+  }
 
   return (
     <div>
       <Title text="give feedback" />
-      <Button handleClick={() => setGood(good + 1)} text="good" />
-      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
-      <Button handleClick={() => setBad(bad + 1)} text="bad" />
+      <Button handleClick={handleGoodClick} text="good" />
+      <Button handleClick={handleNeutralClick} text="neutral" />
+      <Button handleClick={handleBadClick} text="bad" />
       <Title text="statistics" />
       <Display name="good" value={good} />
       <Display name="neutral" value={neutral} />
       <Display name="bad" value={bad} />
+      <Display name="all" value={all}/>
+      <Display name="average" value={(good - bad) / all} />
+      <Display name="positive" value={good / all}/>
     </div>
   )
 }
